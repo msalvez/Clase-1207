7ee0f8b5881bdc5355449041968d9c7b744b0697
Mathias Alvez <mathias@muustack.com>
2021-07-12 19:09:39 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+console.log("Todo funciona correctamente");
+
+//Tareas globales
+//Paso 1
+// Crear todas las variables a llamar en mi proyecto
+
+
+
+//Paso 2
+//Necesito mostrar mis tareas
+
+
+
+//Paso 3
+//Necesito crear mis tareas.
+
+
+//Tareas especificas
+//Detectar envio de tareas
